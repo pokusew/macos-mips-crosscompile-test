4c0b3b9bd2fa160cccbb2473bf94953c28331d14
Martin Endler <pokusew@seznam.cz>
2020-03-08 05:20:42 +0100

    Fix CI

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ set -e
 mkdir -p build
 cd build
 wget http://cmp.felk.cvut.cz/~pisa/apo/qtmips/gcc-mips-elf_7.4_macos.zip
-pwd
+build_dir=$(pwd)
 cd /
-unzip gcc-mips-elf_7.4_macos.zip
+unzip "$build_dir"/gcc-mips-elf_7.4_macos.zip
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 # TODO
+
+build/usr/local/bin/mips-elf-gcc -o src/sum2vars src/sum2vars.S
